fcb91f6f4ba6a86d86d939b1c9322595135ea767
namsuji <52815908+NamSuJi@users.noreply.github.com>
2019-10-29 23:17:26 +0900

    Create fibonachi.c

---
+++ fibonachi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+long long fibo(long long int num)
+{
+    static long long arr[201]={0};
+    
+    if(arr[num]!=0)
+        return arr[num];
+    
+    else if (num==1||num==2)
+        return arr[num]=1;
+    
+    else
+        return arr[num]=(fibo(num-1)+fibo(num-2))%10009;
+}
+
+int main(void)
+{
+    long long int num;
+    scanf("%lld", &num);
+    printf("%lld", fibo(num));
+}
